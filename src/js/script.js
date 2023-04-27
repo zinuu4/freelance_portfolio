99ad03f66d34f50de70f149c78f2e248015fa0a6
zinuu4 <unvm7721@gmail.com>
2023-04-27 12:46:38 +0300

    small improvements and fixes

--- src/js/script.js
+++ src/js/script.js
@@ -90,25 +90,25 @@ counters.forEach( (item, i) => {
     //   $("html, body").animate({scrollTop: $(_href).offset().top+"-x"});
     //   return false;
     // });
-    $("a[href=#up]").on('click', function(event) {
+    // $("a[href=#up]").on('click', function(event) {
   
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+    //   // Make sure this.hash has a value before overriding default behavior
+    //   if (this.hash !== "") {
+    //     // Prevent default anchor click behavior
+    //     event.preventDefault();
   
-        // Store hash
-        var hash = this.hash;
+    //     // Store hash
+    //     var hash = this.hash;
   
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1600, function(){
+    //     // Using jQuery's animate() method to add smooth page scroll
+    //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //     $('html, body').animate({
+    //       scrollTop: $(hash).offset().top
+    //     }, 1600, function(){
   
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+    //       // Add hash (#) to URL when done scrolling (default click behavior)
+    //       window.location.hash = hash;
+    //     });
+    //   } // End if
+    // });
   });
